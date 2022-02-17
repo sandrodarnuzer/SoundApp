c252c644d284288848bb8e2b2f95ec950268702e
Sandro Darnuzer <sandro.darnuzer@gmail.com>
2022-02-17 08:17:27 +0100

    Add fallback if no album

--- pages/album/album_show.php
+++ pages/album/album_show.php
@@ -7,41 +7,47 @@ if (isset($_GET['id'])) {
         'i',
         $album_id,
     );
-    $album = $result->fetch_assoc();
-    $cover_path = get_file_path($album['cover_file'], $album_id);
-
-
-    $result = Database::query(
-        "SELECT song_file, fid_album FROM songs WHERE fid_album=?",
-        'i',
-        $album_id,
-    );
-    $songs = $result->fetch_all(MYSQLI_ASSOC);
-    $songs = array_map(function ($song) {
-        return get_file_path($song['song_file'], $song['fid_album']);
-    }, $songs);
+    if ($result->num_rows > 0) {
+        $album = $result->fetch_assoc();
+        $cover_path = get_file_path($album['cover_file'], $album_id);
+    
+    
+        $result = Database::query(
+            "SELECT song_file, fid_album FROM songs WHERE fid_album=?",
+            'i',
+            $album_id,
+        );
+        $songs = $result->fetch_all(MYSQLI_ASSOC);
+        $songs = array_map(function ($song) {
+            return get_file_path($song['song_file'], $song['fid_album']);
+        }, $songs);
+    }
 }
 ?>
 <main>
 
-    <div>
-        <h1><?= $album['title'] ?></h1>
-        <p><?= $album['description'] ?></p>
-        <img src="<?= $cover_path ?>" alt="Cover" height="100px">
-        <?php foreach ($songs as $index => $song) : ?>
+    <?php if (isset($album)): ?>
+        <div>
+            <h1><?= $album['title'] ?></h1>
+            <p><?= $album['description'] ?></p>
+            <img src="<?= $cover_path ?>" alt="Cover" height="100px">
+            <?php foreach ($songs as $index => $song) : ?>
 
-            <div class="song" data-song="<?=$index + 1?>">
-                <audio>
-                    <source src="<?=$song?>" type="audio/mp3">
-                    Your browser does not support the audio tag.
-                </audio>
-                <span><?=$index?></span>
-                <button class="button-play">Play</button>
-                <button class="button-stop">Stop</button>
-                <button class="button-queue">Add Queue</button>
-            </div>
+                <div class="song" data-song="<?=$index + 1?>">
+                    <audio>
+                        <source src="<?=$song?>" type="audio/mp3">
+                        Your browser does not support the audio tag.
+                    </audio>
+                    <span><?=$index?></span>
+                    <button class="button-play">Play</button>
+                    <button class="button-stop">Stop</button>
+                    <button class="button-queue">Add Queue</button>
+                </div>
 
-        <?php endforeach ?>
-        <button class="button-all">Play all</button>
-    </div>
+            <?php endforeach ?>
+            <button class="button-all">Play all</button>
+        </div>
+    <?php else: ?>
+        <h1>No album</h1>
+    <?php endif ?>
 </main>
