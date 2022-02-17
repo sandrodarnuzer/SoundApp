7dde8c82cbb2e78512f0290d44d9e650675d21b5
Sandro Darnuzer <sandro.darnuzer@gmail.com>
2022-02-17 09:25:03 +0100

    Change upload form

--- pages/album/album_new.php
+++ pages/album/album_new.php
@@ -15,8 +15,32 @@
         <br>
         <label for="albumsongs">Musiktitel</label>
         <br>
-        <input type="file" name="albumsongs[]" id="albumsongs" multiple accept=".mp3, .ogg">
+        <div class="songfiles">
+            
+        </div>
+        <button type="button" id="addsong">Add</button>
         <br>
         <button type="submit" name="createalbum">Hinzufügen</button>
     </form>
 </main>
+<script>
+    let songsAdded = 0;
+    const songFiles = document.querySelector('.songfiles');
+    const buttonAddSong = document.getElementById('addsong');
+
+    buttonAddSong.addEventListener('click', () => {
+        songsAdded++;
+        const songFile = document.createElement('div');
+        songFile.setAttribute('id', `songfile-${songsAdded}`);
+        songFile.innerHTML = `
+            <button type="button" id="button-remove-${songsAdded}" data-songfile="${songsAdded}">Remove</button>
+            <input type="text" name="albumsongname-${songsAdded}" id="albumsongname-${songsAdded}">
+            <input type="file" name="albumsongfile-${songsAdded}" id="albumsongfile-${songsAdded}" multiple accept=".mp3, .ogg">
+        `;
+        songFiles.append(songFile);
+        document.getElementById(`button-remove-${songsAdded}`).addEventListener('click', e => {
+            const songNr = e.target.dataset.songfile;
+            document.getElementById(`songfile-${songNr}`).remove();
+        });
+    });
+</script>
