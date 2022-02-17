2e07e26b4764f26a1c13815c9a53f72a7927dc47
Sandro Darnuzer <sandro.darnuzer@gmail.com>
2022-02-17 11:33:01 +0100

    Add basic styling

--- pages/album/album_new.php
+++ pages/album/album_new.php
@@ -1,41 +1,44 @@
 <main>
     <h1>Neues Album</h1>
-    <form action="create" method="post" enctype="multipart/form-data">
+    <form action="create" method="post" enctype="multipart/form-data" name="albumform">
         <label for="albumtitle">Titel</label>
         <br>
-        <input type="text" name="albumtitle" id="albumtitle">
+        <input type="text" name="albumtitle" id="albumtitle" required>
         <br>
         <label for="albumdescription">Beschreibung</label>
         <br>
-        <textarea name="albumdescription" id="albumdescription" cols="30" rows="10"></textarea>
+        <textarea name="albumdescription" id="albumdescription" cols="30" rows="10" required></textarea>
         <br>
         <label for="albumcover">Album Cover</label>
         <br>
-        <input type="file" name="albumcover" id="albumcover" accept=".jpeg, .jpg, .png, .gif">
+        <input type="file" name="albumcover" id="albumcover" accept=".jpeg, .jpg, .png, .gif" required>
         <br>
         <label for="albumsongs">Musiktitel</label>
+        <button type="button" id="addsong">Add</button>
         <br>
         <input type="hidden" name="songsadded" id="songsadded">
         <div class="songfiles">
             
         </div>
-        <button type="button" id="addsong">Add</button>
         <br>
-        <button type="submit" name="createalbum">Hinzufügen</button>
+        <button class="large" type="button" name="createalbum" id="createalbum">Erstellen</button>
     </form>
 </main>
 <script>
     let songsAdded = 0;
     const songFiles = document.querySelector('.songfiles');
     const buttonAddSong = document.getElementById('addsong');
+    const buttonCreateAlbum = document.getElementById('createalbum');
+    const albumForm = document.forms['albumform'];
 
     buttonAddSong.addEventListener('click', () => {
         const songFile = document.createElement('div');
         songFile.setAttribute('id', `songfile-${songsAdded}`);
+        songFile.setAttribute('class', 'songfile');
         songFile.innerHTML = `
             <button class="button-remove" type="button" id="button-remove-${songsAdded}" data-songfile="${songsAdded}" tabindex="-1">&minus;</button>
-            <input type="text" name="albumsongname-${songsAdded}">
-            <input type="file" name="albumsongfile-${songsAdded}" accept=".mp3, .ogg">
+            <input type="text" name="albumsongname-${songsAdded}" required>
+            <input type="file" name="albumsongfile-${songsAdded}" accept=".mp3, .ogg" required>
         `;
         songFiles.append(songFile);
 
@@ -44,6 +47,11 @@
             document.getElementById(`songfile-${songNr}`).remove();
         });
         songsAdded++;
+    });
+    
+    buttonCreateAlbum.addEventListener('click', () => {
+        if (songFiles.childElementCount < 1 || !albumForm.checkValidity()) return;
         document.getElementById('songsadded').value = songsAdded;
+        albumForm.submit();
     });
 </script>
