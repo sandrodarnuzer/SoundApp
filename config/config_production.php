b332662314bb059bdb38f68bb195e7da70316b7c
Sandro Darnuzer <sandro.darnuzer@gmail.com>
2022-02-18 14:14:39 +0100

    Setup Build & Deployscript

---
+++ config/config_production.php
@@ -0,0 +1,23 @@
+<?php
+class Config {
+    // General
+    const BASE_PATH = '/';
+
+    // Filetypes
+    const IMAGE_TYPES = array(
+        'image/jpeg',
+        'image/jpg',
+        'image/png',
+        'image/gif',
+    );
+    const AUDIO_TYPES = array(
+        'audio/mpeg',
+        'audio/ogg',
+    );
+
+    // Database
+    const DB_HOST = 'localhost';
+    const DB_USER = 'u651054896_soundapp';
+    const DB_PASSWORD = 'f@_nEgMteQ*u-DCc2yQk';
+    const DB_NAME = 'u651054896_soundapp';
+}
---
+++ scripts/build.php
@@ -0,0 +1,49 @@
+<?php
+
+$items = array(
+    'assets',
+    'components',
+    'pages',
+    'functions.php',
+    '.htaccess',
+    'index.php',
+    'index.php',
+    'router.php',
+);
+
+$environments = array(
+    'production',
+    'test',
+);
+
+$output_folder = 'dist';
+
+if ($argc <= 1) {
+    echo "Too few arguments\n";
+    return;
+}
+
+$environment = $argv[1];
+if (!in_array($environment, $environments)) {
+    echo "Unvalid environment\n";
+    return;
+}
+
+$current_dir = getcwd();
+if (!file_exists($current_dir.DIRECTORY_SEPARATOR.'index.php')) {
+    echo "No Index file found\n";
+    return;
+}
+
+$output_path = $current_dir.DIRECTORY_SEPARATOR.$output_folder;
+if (file_exists($output_path)) exec("rm -rf ${output_path}");
+// mkdir($output_path);
+mkdir($output_path.DIRECTORY_SEPARATOR.'config', 0777, true);
+
+$config_source = $current_dir.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config_'.$environment.'.php';
+$config_destination = $output_path.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php';
+copy($config_source, $config_destination);
+foreach ($items as $item) {
+    $item_source = $current_dir.DIRECTORY_SEPARATOR.$item;
+    exec("cp -R ${item_source} ${output_path}");
+}
